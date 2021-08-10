3084733fa1c32cb5ff0df49b46276ec314d7ce8b
chiragsingh1 <imchirag.2717@gmail.com>
2021-08-10 17:30:47 +0530

    design navbar

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navbar from '../Navbar/Navbar';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <span>🎧</span>
+      <span> L Y R I C A L L Y </span>
+      <span>🎧</span>
+    </nav>
+  );
+}
+
+export default Navbar;
